a213a424b9b59729c5ebd0d8fd87a96566b5e9d0
Nayasis <nayasis@gmail.com>
2015-12-16 15:49:39 +0900

    create congifuration xsd and mapper xsd.

---
+++ src/test/java/org/nybatis/core/xml/readAndWrite/NXmlCommonTest.java
@@ -0,0 +1,31 @@
+package org.nybatis.core.xml.readAndWrite;
+
+import org.nybatis.core.conf.Const;
+import org.nybatis.core.log.NLogger;
+import org.nybatis.core.xml.NXml;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.nio.file.Paths;
+
+/**
+ * @author 1002159
+ * @since 2015-12-14
+ */
+public class NXmlCommonTest {
+
+    @BeforeClass
+    public void before() {
+    }
+
+    @Test
+    public void read() {
+
+        NXml xml02 = new NXml( Paths.get( "C:\\TstoreIDE\\workspace\\NayasisCore\\NyBatisCore\\src\\test\\java\\org\\nybatis\\core\\xml\\readAndWrite\\common.xml" ) );
+
+        NLogger.debug( xml02 );
+
+    }
+
+
+}
