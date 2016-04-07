abd958bd1806a27a6c4a0f2d2afeebd338a35493
정화수/Store기술개발팀/SKP <skp.9230085@sk.com>
2016-04-07 11:17:36 +0900

    change NProperties to Wrapper class of Properties.

--- src/main/java/org/nybatis/core/util/NProperties.java
+++ src/main/java/org/nybatis/core/util/NProperties.java
@@ -1,68 +1,94 @@
 package org.nybatis.core.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.nybatis.core.conf.Const;
 import org.nybatis.core.exception.unchecked.IoException;
 import org.nybatis.core.file.FileUtil;
 import org.nybatis.core.worker.WorkerReadLine;
 
+/**
+ * Properties wrapper
+ *
+ */
 public class NProperties {
 
-	private Map<String, String> properties = new Hashtable<>();
+	private Properties properties = new Properties();
 
 	public NProperties() {}
 
+	public NProperties( String filePath ) throws IoException {
+		this( filePath, StandardCharsets.UTF_8.toString() );
+	}
+
+	public NProperties( String filePath, String charset ) throws IoException {
+		readFrom( new File(Const.profile.getFileName(filePath)), charset );
+	}
+
 	public NProperties( File file ) throws IoException {
-	    readFrom( file );
+		this( file,  StandardCharsets.UTF_8.toString() );
+	}
+
+	public NProperties( File file, String charset ) throws IoException {
+	    readFrom( file, charset );
 	}
 
 	public NProperties( NProperties properties ) {
 		append(properties);
 	}
 
-    public NProperties readFrom( File file ) throws IoException {
+	public NProperties readFrom( File file ) throws IoException {
+		return readFrom( file, StandardCharsets.UTF_8.toString() );
+	}
 
-		FileUtil.readFrom( file, readLine -> {
+    public NProperties readFrom( File file, String charset ) throws IoException {
 
-            readLine = readLine.trim();
-
-            if( readLine.startsWith( "#" ) ) return;
-
-            List<String> list = StringUtil.split( readLine, "=" );
-
-            if( list.size() == 0 ) return;
-
-            String key   = list.get( 0 ).trim();
-            String value = "";
-
-            if( list.size() > 1 ) {
-                list.remove( 0 );
-                value = StringUtil.join( list, "" ).trim();
-                value = StringUtil.unescape( value );
-            }
-
-            properties.put( key, value );
-
-        } );
+		try {
+			properties.load( new BufferedReader( new InputStreamReader( new FileInputStream( file ), charset ) ));
+		} catch( IOException e ) {
+			throw new IoException( e );
+		}
 
 		return this;
 
 	}
 
 	public String get( String key ) {
-		return properties.get( key );
+		return properties.getProperty( key );
 	}
 
-	public void set( String key, String value ) {
+	public NProperties set( String key, String value ) {
 		properties.put( key, value );
+		return this;
 	}
 
-	public String remove( String key ) {
-		return properties.remove( key );
+	public <T> T getObject( String key ) {
+		Object val = properties.get( key );
+		return val == null ? null : (T) val;
+	}
+
+	public NProperties setObject( String key, Object value ) {
+		properties.put( key, value );
+		return this;
+	}
+
+	public NProperties remove( String key ) {
+		properties.remove( key );
+		return this;
 	}
 
 	public boolean hasKey( String key ) {
@@ -78,15 +104,12 @@ public class NProperties {
 	}
 
 	public Set<String> keySet() {
-		return properties.keySet();
+		return properties.keySet().stream().map( StringUtil::nvl ).collect( Collectors.toSet() );
 	}
 
 	public NProperties append( NProperties otherProperties ) {
-
 		properties.putAll( otherProperties.properties );
-
 		return this;
-
 	}
 
 	public String toString() {
@@ -97,7 +120,7 @@ public class NProperties {
 		return properties.hashCode();
 	}
 
-	public Map<String, String> toMap() {
+	public Properties getUnwrappedProperties() {
 		return properties;
 	}
 
