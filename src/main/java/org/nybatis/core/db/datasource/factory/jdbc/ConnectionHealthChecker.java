9d8987a6cf366ae55365eb59c93bb8e0f88f3e8e
nayasis <nayasis@gmail.com>
2016-04-26 17:57:42 +0900

    shrink connection pool when idle status keeps for 300 seconds.

--- src/main/java/org/nybatis/core/db/datasource/factory/jdbc/ConnectionHealthChecker.java
+++ src/main/java/org/nybatis/core/db/datasource/factory/jdbc/ConnectionHealthChecker.java
@@ -1,46 +1,117 @@
 package org.nybatis.core.db.datasource.factory.jdbc;
 
-import java.util.Stack;
-import java.util.UUID;
-
 import org.nybatis.core.db.configuration.connectionPool.JdbcDatasourceProperties;
 import org.nybatis.core.db.datasource.proxy.ProxyConnection;
 import org.nybatis.core.log.NLogger;
+import org.nybatis.core.model.LimitedQueue;
+
+import java.sql.SQLException;
+import java.util.UUID;
 
 
+/**
+ * Connection health checker
+ */
 public class ConnectionHealthChecker {
 
-	Stack<ProxyConnection>   idleConnectionPool   = null;
-	JdbcDatasourceProperties datasourceProperties = null;
+	private JdbcDataSource        dataSource    = null;
+	private LimitedQueue<Integer> poolStatistic = new LimitedQueue<>( 10 );
+	private LimitedQueue<Integer> idleStatistic = new LimitedQueue<>( 10 );
 
-	private Thread healthChecker  = new Thread( new Runnable() {
-		public void run() {
-			while( true ) {
-				try { Thread.sleep( 1_000 ); } catch( InterruptedException e ) {}
-				pingConnection();
-			}
-		}
-	}, ConnectionHealthChecker.class.getName() + "." + UUID.randomUUID().toString() );
+	private Thread healthChecker = new Thread( () -> {
+        while( true ) {
+            pingConnections();
+			sleep( 1_000 );
+        }
+    }, generateThreadName("healthChecker") );
 
-	public ConnectionHealthChecker( Stack<ProxyConnection> idleConnectionPool, JdbcDatasourceProperties datasourceProperties ) {
-		this.idleConnectionPool   = idleConnectionPool;
-		this.datasourceProperties = datasourceProperties;
+	private Thread idleConnectionShrinker = new Thread( () -> {
+        while( true ) {
+            shrinkConnections();
+			sleep( 30_000 );
+        }
+    }, generateThreadName("idleConnectionShrinker") );
+
+	private void sleep( long millis ) {
+		try { Thread.sleep( millis ); } catch( InterruptedException e ) {}
+	}
+
+	private String generateThreadName( String name ) {
+		return String.format( "%s.%s.%s", ConnectionHealthChecker.class.getName(), name, UUID.randomUUID().toString() );
+	}
+
+	public ConnectionHealthChecker( JdbcDataSource dataSource ) {
+		this.dataSource = dataSource;
 	}
 
 	public void run() {
-		if( datasourceProperties.isPingEnable() ) {
+		if( dataSource.getDatasourceProperties().isPingEnable() ) {
 			healthChecker.start();
+			idleConnectionShrinker.start();
 			NLogger.trace( "Connection HealthChecker is started." );
 		}
 	}
 
-	private void pingConnection() {
-		for( ProxyConnection connection : idleConnectionPool ) {
-			if( connection.getElpasedTimeAfterLastUsed() <= datasourceProperties.getPingCycle() ) continue;
-			if( connection.ping(datasourceProperties.getPingQuery()) == false ) {
+	private void pingConnections() {
+		for( ProxyConnection connection : dataSource.connectionPoolIdle ) {
+			if( connection.getElpasedTimeAfterLastUsed() <= dataSource.datasourceProperties.getPingCycle() ) continue;
+			if( connection.ping(dataSource.datasourceProperties.getPingQuery()) == false ) {
 				connection.destroy();
 			}
 		}
+	}
+
+	private void shrinkConnections() {
+
+		poolStatistic.add( dataSource.getPoolCount() );
+		idleStatistic.add( dataSource.connectionPoolIdle.size() );
+
+		if( ! hasReliableStatistic() ) return;
+
+		JdbcDatasourceProperties properties = dataSource.getDatasourceProperties();
+
+		int closableCount = getLeastCount( idleStatistic ) -  properties.getPoolMin();
+
+		if( closableCount <= 0 ) return;
+
+		for( int i = 0; i < closableCount; i++ ) {
+			try {
+				ProxyConnection connection = dataSource.getProxyConnection();
+				connection.destroy();
+				dataSource.giveBackConnectionToPool( connection );
+			} catch( SQLException e ) {
+				NLogger.error( e );
+			}
+		}
+
+		NLogger.trace( ">> shrink connecton pool (environmenti:{}, shrinked connection count:{}, current pool count:", properties.getId(), closableCount, dataSource.getPoolCount() );
+
+	}
+
+	private boolean hasReliableStatistic() {
+
+		if( ! poolStatistic.isFull() ) return false;
+
+		int prevCount = poolStatistic.get( 0 );
+
+		for( Integer count : poolStatistic ) {
+			if( count != prevCount ) return false;
+		}
+
+		return true;
+
+	}
+
+	private int getLeastCount( LimitedQueue<Integer> statistic ) {
+
+		int maxCount = Integer.MAX_VALUE;
+
+		for( Integer count : statistic ) {
+			maxCount = Math.min( maxCount, count );
+		}
+
+		return maxCount;
+
 	}
 
 }
