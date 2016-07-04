cf5575117ca8629896c0390e8096b01349ea657e
nayasis <nayasis@gmail.com>
2016-07-04 12:09:03 +0900

    1. add method "getParameters" to SessionExecutor because there is no pointer to designate original parameter.   - parameter can be added via various ways. ( in constructor, attached later, via select keys )     so there is no original parameter in point of SesseionExecutor view. 2. add test code for NDate

--- src/test/java/org/nybatis/core/model/NDateTest.java
+++ src/test/java/org/nybatis/core/model/NDateTest.java
@@ -1,6 +1,12 @@
 package org.nybatis.core.model;
 
+import org.nybatis.core.conf.Const;
+import org.nybatis.core.file.FileUtil;
+import org.nybatis.core.reflection.Reflector;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.Serializable;
 
 import static org.testng.Assert.assertEquals;
 
@@ -36,6 +42,61 @@ public class NDateTest {
 
         assertEquals( beginningOfMonthDate.getMillisecond(), 0 );
         assertEquals( endOfMonthDate.getMillisecond(), 999 );
+
+    }
+
+    @Test
+    public void serializeViaFile() {
+
+        String before, after;
+
+        SampleVo sampleVo = new SampleVo( "nayasis", new NDate( "1977-01-22" ) );
+
+        before = sampleVo.toString();
+
+        String testFile = Const.path.getBase() + "/test.serialized.obj";
+
+        FileUtil.writeObject( testFile, sampleVo );
+
+        sampleVo = FileUtil.readObject( testFile );
+
+        after = sampleVo.toString();
+
+        Assert.assertEquals( after, before );
+
+        FileUtil.delete( testFile );
+
+    }
+
+    private static class SampleVo implements Serializable {
+
+        private String name;
+        private NDate  birth;
+
+        public SampleVo( String name, NDate birth ) {
+            this.name = name;
+            this.birth = birth;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName( String name ) {
+            this.name = name;
+        }
+
+        public NDate getBirth() {
+            return birth;
+        }
+
+        public void setBirth( NDate birth ) {
+            this.birth = birth;
+        }
+
+        public String toString() {
+            return Reflector.toJson( this );
+        }
 
     }
 
