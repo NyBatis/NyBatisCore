30ba922bc6ebb70b05d4780c91d1c9abfa7534a2
nayasis <nayasis@gmail.com>
2017-06-09 20:11:55 +0900

    allow '%m' logback pattern flag in NLogger.

--- src/main/java/org/nybatis/core/log/converter/CallerConverterForLine.java
+++ src/main/java/org/nybatis/core/log/converter/CallerConverterForLine.java
@@ -6,9 +6,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 public class CallerConverterForLine extends ClassicConverter {
 
 	public String convert( ILoggingEvent event ) {
-
 		return new StackTracer( event ).getLineNumber();
-	
 	}
 
 }
