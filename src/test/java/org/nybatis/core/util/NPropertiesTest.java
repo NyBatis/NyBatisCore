3fac0d0d8e5c362539d1e2d03a3e64219555d6e8
1000084 <nayasis@onestorecorp.com>
2017-08-30 11:07:51 +0900

    add test case on NProperties

--- src/test/java/org/nybatis/core/util/NPropertiesTest.java
+++ src/test/java/org/nybatis/core/util/NPropertiesTest.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 
 import org.nybatis.core.conf.Const;
 import org.nybatis.core.log.NLogger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NPropertiesTest {
 
 	@Test
 	public void read() throws IOException {
-
-		NProperties properties = new NProperties( new File( Const.path.getConfigDatabase() + "/config.properties") );
-
-		NLogger.debug( properties );
-
+		NProperties properties = new NProperties( new File( Const.path.getConfig() + "/properties/test.properties") );
+		NLogger.debug( ">> Test properties :\n{}", properties );
+		Assert.assertEquals("firstLine,SecondLine", properties.get("url") );
 	}
 
 }
