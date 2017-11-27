50ee4b4cbe706b694eff11a411c8dec71c13cbad
nayasis <wjdghktn0666>
2017-11-28 02:12:41 +0900

    refactor ColumnAnnotationInspector

--- src/main/java/org/nybatis/core/reflection/inspector/ColumnAnnotationInspector.java
+++ src/main/java/org/nybatis/core/reflection/inspector/ColumnAnnotationInspector.java
@@ -1,15 +1,13 @@
 package org.nybatis.core.reflection.inspector;
 
 import com.fasterxml.jackson.databind.introspect.Annotated;
-import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import org.nybatis.core.db.annotation.Column;
-import org.nybatis.core.log.NLogger;
-import org.nybatis.core.reflection.deserializer.ColumnBooleanDeserializer;
 import org.nybatis.core.reflection.deserializer.ColumnBeanDeserializer;
-import org.nybatis.core.reflection.serializer.column.ColumnBooleanSerializer;
+import org.nybatis.core.reflection.deserializer.ColumnBooleanDeserializer;
 import org.nybatis.core.reflection.serializer.column.ColumnBeanSerializer;
+import org.nybatis.core.reflection.serializer.column.ColumnBooleanSerializer;
 import org.nybatis.core.util.Types;
 
 /**
@@ -20,67 +18,59 @@ import org.nybatis.core.util.Types;
  */
 public class ColumnAnnotationInspector extends JacksonAnnotationIntrospector {
 
-//    @Override
-//    public boolean hasIgnoreMarker( AnnotatedMember member ) {
-//        return member.getAnnotation( Column.class ) == null;
-//    }
-
-
-
     @Override
     public Object findSerializer( Annotated annotated ) {
 
-        if( ! annotated.hasAnnotation( Column.class ) ) {
-            return super.findContentSerializer( annotated );
+        if( ! annotated.hasAnnotation(Column.class) || ! isStringType(annotated) ) {
+            return super.findSerializer( annotated );
         }
 
-        Column annotation = getAnnotation( annotated );
         Class classType = annotated.getRawType();
 
-        switch( annotation.type() ) {
-            case java.sql.Types.VARCHAR :
-            case java.sql.Types.CHAR :
-            case java.sql.Types.CLOB :
-            case java.sql.Types.NCHAR :
-            case java.sql.Types.NCLOB :
-            case java.sql.Types.LONGVARBINARY :
-            case java.sql.Types.LONGNVARCHAR :
-
-                if( Types.isNotString(classType) ) {
-                    if( Types.isBoolean(classType) ) {
-                        return ColumnBooleanSerializer.class;
-                    } else if( Types.isPrimitive(classType) ) {
-                        return super.findContentSerializer( annotated );
-                    } else {
-                        return ColumnBeanSerializer.class;
-                    }
-                }
-
+        if( Types.isNotString(classType) ) {
+            if( Types.isBoolean(classType) ) {
+                return ColumnBooleanSerializer.class;
+            } else if( ! Types.isPrimitive(classType) ) {
+                return ColumnBeanSerializer.class;
+            }
         }
 
-        return super.findContentSerializer( annotated );
+        return super.findSerializer( annotated );
 
     }
 
     @Override
     public Object findDeserializer( Annotated annotated ) {
 
-        if( ! annotated.hasAnnotation( Column.class ) ) {
-            return super.findContentDeserializer( annotated );
+        if( ! annotated.hasAnnotation(Column.class) || ! isStringType(annotated) ) {
+            return super.findDeserializer( annotated );
         }
-        Column annotation = getAnnotation( annotated );
-        Class classType = null;
 
+        Class classType;
         try {
             classType = ( (AnnotatedMethod) annotated ).getRawParameterType( 0 );
         } catch( Exception e ) {
-            return super.findContentDeserializer( annotated );
+            return super.findDeserializer( annotated );
         }
 
-        if( classType == null ) {
-            return super.findContentDeserializer( annotated );
+        if( Types.isNotString(classType) ) {
+            if( Types.isBoolean(classType) ) {
+                return ColumnBooleanDeserializer.class;
+            } else if( ! Types.isPrimitive(classType) ) {
+                return ColumnBeanDeserializer.class;
+            }
         }
 
+        return super.findDeserializer( annotated );
+
+    }
+
+    private Column getAnnotation( Annotated annotated ) {
+        return annotated.getAnnotation( Column.class );
+    }
+
+    private boolean isStringType( Annotated annotated ) {
+        Column annotation = getAnnotation( annotated );
         switch( annotation.type() ) {
             case java.sql.Types.VARCHAR :
             case java.sql.Types.CHAR :
@@ -89,31 +79,9 @@ public class ColumnAnnotationInspector extends JacksonAnnotationIntrospector {
             case java.sql.Types.NCLOB :
             case java.sql.Types.LONGVARBINARY :
             case java.sql.Types.LONGNVARCHAR :
-
-                if( Types.isNotString(classType) ) {
-                    if( Types.isBoolean(classType) ) {
-                        return ColumnBooleanDeserializer.class;
-                    } else if( Types.isPrimitive(classType) ) {
-                        return super.findContentDeserializer( annotated );
-                    } else {
-                        return ColumnBeanDeserializer.class;
-                    }
-                }
-
+                return true;
         }
-
-        return super.findContentDeserializer( annotated );
-
-    }
-
-    @Override
-    public Object findSerializationContentConverter( AnnotatedMember a ) {
-        NLogger.debug( a );
-        return super.findSerializationContentConverter( a );
-    }
-
-    private Column getAnnotation( Annotated annotated ) {
-        return annotated.getAnnotation( Column.class );
+        return false;
     }
 
 }
