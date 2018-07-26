eb86a1358c8bf88842837649a505dfc0b31d20b2
1000084 <nayasis@onestorecorp.com>
2018-07-26 11:01:15 +0900

    add StringUtil.similarity

--- src/main/java/org/nybatis/core/util/StringUtil.java
+++ src/main/java/org/nybatis/core/util/StringUtil.java
@@ -1451,4 +1451,66 @@ public class StringUtil {
 		return sb.toString();
 	}
 
+
+	/**
+	 * get similarity between 0 and 1.
+	 *
+	 * 0 is non-matched and 1 is perfect-matched.
+	 *
+	 * @param source
+	 * @param target
+     * @return
+     */
+	public static double similarity( String source, String target ) {
+
+		String longer = nvl(source), shorter = nvl(target);
+		if( longer.length() < shorter.length() ) {
+			String temp = longer;
+			longer = shorter; shorter = temp;
+		}
+		int longerLength = longer.length();
+		if (longerLength == 0) return 1.0;
+
+		return (longerLength - getLavenshteinDistance(longer, shorter)) / (double) longerLength;
+
+	}
+
+	/**
+	 * get Levenshtein distance
+	 *
+	 * @param source
+	 * @param target
+     * @return
+	 * @see http://rosettacode.org/wiki/Levenshtein_distance#Java
+	 * @see https://en.wikipedia.org/wiki/Levenshtein_distance
+     */
+	private static int getLavenshteinDistance( String source, String target ) {
+
+		source = toLowerCase( source );
+		target = toLowerCase( target );
+
+		int[] costs = new int[ target.length() + 1 ];
+
+		for (int i = 0; i <= source.length(); i++) {
+			int lastValue = i;
+			for (int j = 0; j <= target.length(); j++) {
+				if (i == 0) {
+					costs[j] = j;
+				} else {
+					if (j > 0) {
+						int newValue = costs[j-1];
+						if (source.charAt(i - 1) != target.charAt(j - 1))
+							newValue = Math.min( Math.min(newValue, lastValue), costs[j]) + 1;
+						costs[j-1] = lastValue;
+						lastValue = newValue;
+					}
+				}
+			}
+			if ( i > 0 )
+				costs[target.length()] = lastValue;
+		}
+		return costs[target.length()];
+	}
+
+
 }
